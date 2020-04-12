5a37ef890c6c53ffa77ece4ddeed656b8898e254
tessamg18 <60265321+tessamg18@users.noreply.github.com>
2020-04-12 16:26:53 -0600

    Update KMeansClustering_functions.py

--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -9,8 +9,10 @@ def openckdfile():
     glucose, hemoglobin, classification = np.loadtxt('ckd.csv', delimiter=',', skiprows=1, unpack=True)
     return glucose, hemoglobin, classification
 
+
 def select(K):
     return np.random.random((K, 2))
+
 
 def assign(centroids, hemoglobin, glucose):
     K = centroids.shape[0]
@@ -19,32 +21,51 @@ def assign(centroids, hemoglobin, glucose):
         g = centroids[i,1]
         h = centroids[i,0]
         distances[i] = np.sqrt((hemoglobin-h)**2+(glucose-g)**2)
-    assignments = np.argmin(distances, axis = 0)    
-    print(assignments)
+    print(distances)
+    assignments = np.argmin(distances, axis = 0) 
+    print(assignments) 
     return None
 
+ 
 def update(assignments, hemoglobin, glucose, centroids):
-    sortassignments = np.argsort(assignments)
+#    sortassignments = np.argsort(assignments)
     K = centroids.shape[0]
     updatedcen = np.zeros((K, 2))
     for i in range(K):
-        if sortassignments == i:
-            updatedcen[10] = np.mean(glucose[sortassignments==i])
-            updatedcen[i] = np.mean(hemoglobin[sortassignments==i])
-    return updatedcen
+        hmean = np.mean(hemoglobin[assignments==i])
+        gmean = np.mean(glucose[assignments==i])
+        updatedcen[i,0] = hmean
+        updatedcen[i,1] = gmean
+        updated_centroids = updatedcen
+    print(updated_centroids)
+    return updated_centroids
+
+
+
+
+
+
 
 
 glucose, hemoglobin, classification = openckdfile()
 
+
+centroids = select(2)
+assignments = assign(centroids, hemoglobin, glucose)
+updated_centroids = update(assignments, hemoglobin, glucose, centroids)
+print(update(assignments, hemoglobin, glucose, centroids))
+
+
+
+
+
+
+
 plt.figure()
+plt.plot(updated_centroids, "gs")
 plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
 plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
 plt.xlabel("Hemoglobin")
 plt.ylabel("Glucose")
 plt.legend()
 plt.show()
-
-
-centroids = select(10)
-assignments = assign(centroids, hemoglobin, glucose)
-print(update(assignments, hemoglobin, glucose, centroids))
