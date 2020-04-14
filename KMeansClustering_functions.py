4eab52f8e93fa16c4f1aab2ffba6cda487887e0d
tessamg18 <60265321+tessamg18@users.noreply.github.com>
2020-04-13 18:11:43 -0600

    Update KMeansClustering_functions.py

--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -11,33 +11,32 @@ def openckdfile():
 
 
 def select(K):
-    return 10*np.random.random((K, 2)) #creates a multi-D array of 2 elements each (k rows) 
+    return np.random.random((K, 2)) #creates a multi-D array of 2 elements each (k rows) 
 
 
-def assign(centroids, hemoglobin, glucose):
+def assign(centroids, hscaled, gscaled):
     K = centroids.shape[0]
-    distances = np.zeros((K, len(hemoglobin)))
+    distances = np.zeros((K, len(hscaled)))
     for i in range(K):
         g = centroids[i,1]
         h = centroids[i,0]
-        distances[i] = np.sqrt((hemoglobin-h)**2+(glucose-g)**2)
+        distances[i] = np.sqrt((hscaled-h)**2+(gscaled-g)**2)
     assignments = np.argmin(distances, axis = 0)
     print(assignments) 
     return assignments
 
  
-def update(assignments, hemoglobin, glucose, centroids):
+def update(assignments, hscaled, gscaled, centroids):
     K = centroids.shape[0]
     updatedcen = np.zeros((K, 2))
-    print("cenn", updatedcen)
     count= 0
     glucsum = 0
     hemosum = 0
     for i in range(K):
         for j in range(len(assignments)):
             if i == assignments[j]:
-                glucsum += glucose[j]
-                hemosum += hemoglobin[j]
+                glucsum += gscaled[j]
+                hemosum += hscaled[j]
                 count+=1
         if count != 0:
             hmean = hemosum/count
@@ -45,34 +44,37 @@ def update(assignments, hemoglobin, glucose, centroids):
             updatedcen[i,0] = hmean
             updatedcen[i,1] = gmean
     updated_centroids = updatedcen
-    print("cen", updatedcen)
     return updated_centroids
     
     
-#    for i in range(len(centroids)):
-#        new_dist = []
-#        for index, assign in enumerate(assignments):
-#            if assign == i:
-#                 new_dist.append([hemoglobin[index], glucose[index]])
-#        print("new dist: ", new_dist)
-#    return centroids
-
-
-#then walk through the centroids and find every distance that is assigned to that centroid
-#then average the distance
-#the average becomes the new centroid
+def iterate(iterations, assignments, hscaled, gscaled, centroids):
+    while iterations >= 1:
+        assignments = assign(centroids, hscaled, gscaled)
+        centroids = update(assignments, hscaled, gscaled, centroids)
+        iterations -= 1
+    return centroids
+    
 
 
 glucose, hemoglobin, classification = openckdfile()
+hscaled = (hemoglobin-3.1)/(17.8-3.1)
+gscaled = (glucose-70)/(490-70)
 
 
+centroids = select(2)
+assignments = assign(centroids, hscaled, gscaled)
+updated_centroids = update(assignments, hscaled, gscaled, centroids)
+final_centroids = iterate(20, assignments, hscaled, gscaled, centroids)
 
-centroids = select(10)
-assignments = assign(centroids, hemoglobin, glucose)
-updated_centroids = update(assignments, hemoglobin, glucose, centroids)
-print(update(assignments, hemoglobin, glucose, centroids))
 
-
+plt.figure()
+plt.plot(final_centroids[:,0], centroids[:,1], "gs")
+plt.plot(hscaled[classification==1],gscaled[classification==1], "k.", label = "Class 1")
+plt.plot(hscaled[classification==0],gscaled[classification==0], "r.", label = "Class 0")
+plt.xlabel("Hemoglobin")
+plt.ylabel("Glucose")
+plt.legend()
+plt.show()
 
 plt.figure()
 plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
