fd85ff999cdfa0beaa874f4318268c0473581508
tessamg18 <60265321+tessamg18@users.noreply.github.com>
2020-04-13 23:47:02 -0600

    i'm dying

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -1,7 +1,27 @@
-#Please place your FUNCTION code for step 4 here.
+#Please place your code for step 4 here.
 
 import KMeansClustering_functions as kmc #Use kmc to call your functions
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-import statistics
+
+glucose, hemoglobin, classification = kmc.openckdfile()
+hscaled = (hemoglobin-3.1)/(17.8-3.1)
+gscaled = (glucose-70)/(490-70)
+
+
+centroids = kmc.select(2)
+assignments = kmc.assign(centroids, hscaled, gscaled)
+updated_centroids = kmc.update(assignments, hscaled, gscaled, centroids)
+final_centroids = kmc.iterate(50, assignments, hscaled, gscaled, centroids)
+kmc.graphingKMeans(glucose, hemoglobin, assignments, centroids)
+
+
+plt.figure()
+plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
+plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
+plt.xlabel("Hemoglobin")
+plt.ylabel("Glucose")
+plt.legend()
+plt.show()
+
--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -5,16 +5,27 @@ import matplotlib.pyplot as plt
 import random
 import statistics
 
+
 def openckdfile():
+#The purpose of this function is to open the file with the training set
+#it does not take any parameters
+#it returns glucose, hemoglobin and classification as arrays
+   #unpack=True makes each column an array
     glucose, hemoglobin, classification = np.loadtxt('ckd.csv', delimiter=',', skiprows=1, unpack=True)
     return glucose, hemoglobin, classification
 
 
 def select(K):
-    return np.random.random((K, 2)) #creates a multi-D array of 2 elements each (k rows) 
+#The purpose of this function is to randomly select a K numbers between 0.0 and 1.0.
+#It takes the parameter K, which the quantity of numbers you want to get
+#these values are the initial centroids, and the function returns the random value
+    return np.random.random((K, 2))
 
 
 def assign(centroids, hscaled, gscaled):
+#The purpose of this function is to assign all of the data points to the nearest centroid
+#it takes the parameters hscaled, gscaled, and centroids
+#then it returns which centroid the points are assigned to 
     K = centroids.shape[0]
     distances = np.zeros((K, len(hscaled)))
     for i in range(K):
@@ -27,6 +38,9 @@ def assign(centroids, hscaled, gscaled):
 
  
 def update(assignments, hscaled, gscaled, centroids):
+#the purpose of this function is update the centroid to the average location of all its points
+#it takes the parameters assignments, hscaled, gscaled, and centroids
+#It returns the new locations of the centroids
     K = centroids.shape[0]
     updatedcen = np.zeros((K, 2))
     count= 0
@@ -48,38 +62,28 @@ def update(assignments, hscaled, gscaled, centroids):
     
     
 def iterate(iterations, assignments, hscaled, gscaled, centroids):
+#The purpose of this function is to continue updating the locations of the centroids
+#It has the parameters iterations, assignments, hscaled, gscaled, and centroids
+#It calls on the assign function and update function a specific number of times that
+#is determined by the iterations
+#It returns the final location of the centroids
     while iterations >= 1:
         assignments = assign(centroids, hscaled, gscaled)
         centroids = update(assignments, hscaled, gscaled, centroids)
         iterations -= 1
     return centroids
-    
 
 
-glucose, hemoglobin, classification = openckdfile()
-hscaled = (hemoglobin-3.1)/(17.8-3.1)
-gscaled = (glucose-70)/(490-70)
-
-
-centroids = select(2)
-assignments = assign(centroids, hscaled, gscaled)
-updated_centroids = update(assignments, hscaled, gscaled, centroids)
-final_centroids = iterate(20, assignments, hscaled, gscaled, centroids)
-
-
-plt.figure()
-plt.plot(final_centroids[:,0], centroids[:,1], "gs")
-plt.plot(hscaled[classification==1],gscaled[classification==1], "k.", label = "Class 1")
-plt.plot(hscaled[classification==0],gscaled[classification==0], "r.", label = "Class 0")
-plt.xlabel("Hemoglobin")
-plt.ylabel("Glucose")
-plt.legend()
-plt.show()
-
-plt.figure()
-plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
-plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
-plt.xlabel("Hemoglobin")
-plt.ylabel("Glucose")
-plt.legend()
-plt.show()
+def graphingKMeans(glucose, hemoglobin, assignments, centroids):
+#the purpose of this function is to graph the training data with the centroid classifications
+#It takes the paramters glucose, hemoglobin, assignments, and centroids
+#it does not return anything
+    plt.figure()
+    for i in range(assignments.max()+1):
+        rcolor = np.random.rand(3,)
+        plt.plot(hscaled[assignments==i],gscaled[assignments==i], ".", label = "Class " + str(i), color = rcolor)
+        plt.plot(centroids[i, 0], centroids[i, 1], "D", label = "Centroid " + str(i), color = rcolor)
+    plt.xlabel("Hemoglobin")
+    plt.ylabel("Glucose")
+    plt.legend()
+    plt.show()
